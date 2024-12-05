be6593d3bc9bbb402e9184349a8fc0fa627e35ac
Thomas Octigan <tom.o@canva.com>
2024-12-05 21:58:30 +1100

    Add files via upload

--- day1.lisp
+++ day1.lisp
@@ -5,27 +5,21 @@
   (loop for line in (uiop:read-file-lines fname)
 	for raw-nums = (ppcre:split "\\s+" line)
 	collect (mapcar #'parse-integer raw-nums)))
-(defun transpose (xs)
-  (loop with as = '()
-	with bs = '()
-	for (x y) in xs
-	do (push x as) (push y bs)
-	finally (return (list as bs))))
-(defun absdiff (a b) (abs (- a b)))
-(defun diffs (ls) (mapcar 'absdiff (first ls) (second ls)))
-(defun sorted (xs) (sort xs #'<))
-(defun sum (xs) (reduce #'+ xs))
+
+(defun unzip (pairs)
+  (loop with xs = '()
+	with ys = '()
+	for (x y) in pairs
+	do (push x xs) (push y ys)
+	finally (return `(,xs ,ys))))
 
 ;; Part 1
-(sum (diffs (mapcar 'sorted (transpose (read-pairs "day1.input")))))
-
+(loop with nums = (unzip (read-pairs "day1.input"))
+      for x in (sort (first nums) #'<)
+      for y in (sort (second nums) #'<)
+      sum (abs (- x y)))
 ;; Part 2
-(loop with nums = (transpose (read-pairs "day1.input"))
-      with xs = (first nums)
-      with ys = (second nums)
-      with similarity = 0
-      for x in xs
-      for freq = (count x ys)
-      for ds = (* x freq)
-      do (incf similarity ds)
-      finally (return similarity))
+(loop with nums = (unzip (read-pairs "day1.input"))
+      for x in (first nums)
+      for freq = (count x (second nums))
+      sum (* x freq))
