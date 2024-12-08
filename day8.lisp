bac3a13237d685505a21b33e0f1c8437c198f69e
Thomas Octigan <tom.o@canva.com>
2024-12-08 22:37:45 +1100

    Add files via upload

---
+++ day8.lisp
@@ -0,0 +1,80 @@
+(require 'uiop)
+(require 'cl-ppcre)
+(defmacro ^ (&rest forms) `(lambda ,@forms))
+
+(defun parse (input)
+  (loop
+    with table = (make-hash-table :test 'equal)
+    with height = (length input)
+    for row in input
+    for y from 0
+    do (loop for c across row
+	     for x from 0
+	     do (setf (gethash `(,x ,y) table) c))
+    finally (return table)))
+
+(defun collect-freqs (table)
+  (loop with freqs = (make-hash-table :test 'equal)
+	for pos being the hash-key using (hash-value cell) of table
+	when (ppcre:scan "\\w" (format nil "~A" cell))
+          do (setf (gethash cell freqs) (append `(,pos) (gethash cell freqs)))
+	finally (return freqs)))
+
+(defun pairs (xs)
+  (loop for (x . rest) on xs nconc (loop for y in rest collect `(,x ,y))))
+
+(defun antinode (ps)
+  (destructuring-bind ((x1 y1) (x2 y2)) ps
+    (let ((dx (- x2 x1))
+	  (dy (- y2 y1)))
+      (list (list (- x1 dx) (- y1 dy))
+	    (list (+ x2 dx) (+ y2 dy))))))
+
+(defun inside-p (p x1 y1 x2 y2)
+  (destructuring-bind (x y) p
+    (and (<= x1 x) (<= x x2)
+	 (<= y1 y) (<= y y2))))
+
+;; part 1
+(length
+ (remove-duplicates
+  (remove-if
+   (^ (p) (not (inside-p p 0 0 49 49)))
+   (loop
+     with table = (parse (uiop:read-file-lines "day8.input"))
+     with freqs = (collect-freqs table)
+     for freq being the hash-key using (hash-value coords) of freqs
+     nconc (mapcan 'antinode (pairs coords))))
+  :test 'equal))
+
+;; part 2
+(defun add (p1 p2)
+  (destructuring-bind ((x1 y1) (x2 y2)) (list p1 p2)
+    `(,(+ x1 x2) ,(+ y1 y2))))
+(defun sub (p1 p2)
+  (destructuring-bind ((x1 y1) (x2 y2)) (list p1 p2)
+    `(,(- x2 x1) ,(- y2 y1))))
+(defun antinode-2 (ps w h)
+  (destructuring-bind (p1 p2) ps
+    (remove-duplicates
+     (nconc
+      (list p1 p2)
+      (loop with dp = (sub p1 p2)
+	    repeat 100
+	    for p = (add p1 dp) then (add p dp)
+	    when (inside-p p 0 0 w h) collect p)
+      (loop with dp = (sub p2 p1)
+	    repeat 100
+	    for p = (add p1 dp) then (add p dp)
+	    when (inside-p p 0 0 w h) collect p))
+     :test 'equal)))
+(length
+ (remove-duplicates
+  (remove-if
+   (^ (p) (not (inside-p p 0 0 49 49)))
+   (loop
+     with table = (parse (uiop:read-file-lines "day8.input"))
+     with freqs = (collect-freqs table)
+     for freq being the hash-key using (hash-value coords) of freqs
+     nconc (mapcan (^ (ps) (antinode-2 ps 49 49)) (pairs coords))))
+  :test 'equal))
